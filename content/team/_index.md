c243bec6ab642b3c88beaa95914461c7eda98288
Yo Yehudi <yochannah@gmail.com>
2017-10-03 13:43:51 +0100

    I think the nav bar is finally functional

--- content/team/_index.md
+++ content/team/_index.md
@@ -1,6 +1,9 @@
 {
   "layout" : "team",
-  "title": "Cambridge team"
+  "title": "About InterMine"
 }
 
+<p>InterMine was formed in 2002 at the University of Cambridge, originally as a Drosophila-dedicated resource, before expanding to become organism-agnostic, enabling a large range of organisations around the world to create their own InterMines. The InterMine team is part of <a href="http://www.micklemlab.org/">Micklem lab</a>, headed by Gos Micklem.</p>
+
+<h3>Meet the team</h3>
 InterMine's core team members are all based in Cambridge and are generally available during <a href="https://time.is/Cambridge">UK daytime hours</a>.
--- static/js/menu.js
+++ static/js/menu.js
@@ -1,54 +1,108 @@
-document.addEventListener("DOMContentLoaded", function () {
+var x = function () {
+    document.addEventListener("DOMContentLoaded", function () {
 
-    var navMenu = document.querySelectorAll(".header-collapsible-nav > li");
-
-    function hideMenu(elem) {
-        elem.classList.remove("show");
-    }
-
-    function showMenu(elem) {
-        elem.classList.add("show");
-    }
-
-    function toggleMenu(elem) {
-        elem.classList.toggle("show");
-    }
-
-    navMenu.forEach(function (menu) {
-        var dropdown = menu.querySelector("ul");
+        var navMenu = document.querySelectorAll(".header-collapsible-nav > li");
         var header = document.querySelector("header");
 
-        //listen for hovers
-        menu.addEventListener("mouseover", function () {
-            if (!this.classList.contains("navigation-menu-link")) {
-                showMenu(dropdown);
-                //hide after we leave
-                menu.addEventListener("mouseout", function () {
-                    hideMenu(dropdown);
-                });
-            } 
-        });
+        function hideSubMenu(elem) {
+            elem.classList.remove("show");
+        }
 
-        //for touch.
-        menu.addEventListener("click", function () {
-            if (!this.classList.contains("navigation-menu-link")) {
-                //they've clicked one of the actual elements
-                if(!this.classList.contains("open")) {
-                    this.focus(); 
-                    this.classList.add("open");
-                    showMenu(dropdown);
-                } else {
-                    this.classList.remove("open");                    
-                    hideMenu(dropdown);                    
+        function showSubMenu(elem) {
+            elem.classList.add("show");
+        }
+
+        function showTouchMenu(elem) {
+            header.classList.add("menu-open")
+            header.querySelectorAll(".categories").forEach(function (li) {
+                li.classList.add("show");
+            });
+        }
+
+        function hideTouchMenu(elem) {
+            header.classList.remove("menu-open")
+            header.querySelectorAll(".categories").forEach(function (li) {
+                li.classList.remove("show");
+            });
+        }
+
+        navMenu.forEach(function (menu) {
+            var dropdown = menu.querySelector("ul");
+
+            //listen for hovers
+            menu.addEventListener("mouseover", function () {
+                if (!this.classList.contains("navigation-menu-link")) {
+                    showSubMenu(dropdown);
+                    //hide after we leave
+                    menu.addEventListener("mouseout", function () {
+                        hideSubMenu(dropdown);
+                    });
                 }
-            } else {
-                //they've clicked the hamburger to open menu
-                header.classList.add("menu-open")
-                header.querySelectorAll(".categories").forEach(function(li){
-                    li.classList.toggle("show");
-                });
-            }
-        });
-    });
+            });
 
-});
+            //listen for keyboard focus
+            menu.addEventListener("focus", function () {
+                if (!this.classList.contains("navigation-menu-link")) {
+                    //they've clicked one of the actual elements
+                    showTouchMenu();
+                    if (!this.classList.contains("open")) {
+                        this.classList.add("open");
+                        showSubMenu(dropdown);
+                    } else {
+                        this.classList.remove("open");
+                        hideSubMenu(dropdown);
+                    }
+                } else {
+                    //they've clicked the hamburger to open menu
+                    showTouchMenu();
+                }
+                menu.addEventListener("blur", function () {
+                    hideTouchMenu();
+                    this.classList.remove("open");
+                    hideSubMenu(dropdown);
+                });
+
+                //keyboard controls for the menu. may need to remove events too
+                var keydown = menu.addEventListener("keydown", function (e) {
+                    var k = e.keyCode,
+                        down = 40;
+                    up = "38";
+
+                    if (k == down) {
+                        //TODO move down
+                    } else {
+                        if (k == up) {
+                            //TODO MOVE UP
+                        }
+                    }
+
+                });
+
+            });
+
+            //for touch.
+            menu.addEventListener("click", function () {
+                if (!this.classList.contains("navigation-menu-link")) {
+                    //they've clicked one of the actual elements
+                    if (!this.classList.contains("open")) {
+                        this.focus();
+                        this.classList.add("open");
+                        showSubMenu(dropdown);
+                    } else {
+                        this.classList.remove("open");
+                        hideSubMenu(dropdown);
+                    }
+                } else {
+                    //they've clicked the hamburger to open menu
+                    showTouchMenu();
+                }
+            });
+        });
+
+        //close button for touch
+        document.getElementById("closeNav").addEventListener("click", function () {
+            hideTouchMenu();
+        });
+
+    });
+}();
